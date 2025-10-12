44b50ef737a05c1a2c61b568b4a7338fac459f09
ryanjchang <ryanjchang@gmail.com>
2025-10-12 13:04:35 -0400

    started profile

--- project-series/src/App.jsx
+++ project-series/src/App.jsx
@@ -1,35 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import header from "./Header"
+import Form from "./Form"
+import Output from "./Output"
+
+export default function App() {
+  const [photo, setPhoto] = useState(null);
+  const [name, setName] = useState("");
+  const [title, setTitle] = useState("");
+  const [fact, setFact] = useState("");
+
+  const [showOutput, setShowOutput] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header/>
+      <Form
+				photo={photo} setPhoto={setPhoto}
+				name={name} setName={setName}
+				title={title} setTitle={setTitle}
+				fact={fact} setFact={setFact}
+				showOutput={showOutput} setShowOutput={setShowOutput}
+			/>
+      <Output photo={photo} name={name} title={title} fact={fact} />
+      
+    </div>
   )
 }
-
-export default App
---
+++ project-series/src/Header.js
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <>
+            <h1>Profile Generator</h1>
+            <p1>Generate your profile</p1>
+        </>
+    );
+}
